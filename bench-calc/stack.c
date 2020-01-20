bae3114c1dd6a484e1fb0e4e29a707e22246ddc5
rowlanja <55389732+rowlanja@users.noreply.github.com>
2020-01-20 13:18:08 +0000

    Bench Calculator in C

---
+++ bench-calc/stack.c
@@ -0,0 +1,12 @@
+#include "stack.h"
+
+struct double_stack * double_stack_new(int max_size) {
+}
+
+// push a value onto the stack
+void double_stack_push(struct double_stack * this, double value) {
+}
+
+// pop a value from the stack
+double double_stack_pop(struct double_stack * this) {
+}
